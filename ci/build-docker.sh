78980712dd9674cdaa5703b8d19220b05290ed9f
Georgi Nikolov <ggeorgi60@gmail.com>
2023-11-02 22:55:01 +0200

    Removed unnecessary scripts

--- ci/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-[[ -z "${GIT_COMMIT}" ]] && Tag="local" || Tag="${GIT_COMMIT::8}"
-[[ -z "${docker_username}" ]] && DockerRepo="" || DockerRepo="ghcr.io/${docker_username}/"
-docker build -t "${DockerRepo}flask-app:latest" -t "${DockerRepo}flask-app:1.0-$Tag" app/
-
